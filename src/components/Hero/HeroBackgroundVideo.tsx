d8a60d85cad0bd7ce95a8903b9cea1271b290483
Antonio F. Gasco <antoniofgasco@gmail.com>
2024-09-17 11:38:34 +0100

    Fixed issue with video route

--- src/components/Hero/HeroBackgroundVideo.tsx
+++ src/components/Hero/HeroBackgroundVideo.tsx
@@ -2,7 +2,7 @@ const HeroBackgroundVideo = () => {
   return (
     <div className="video-container">
       <video className="background-video" autoPlay muted loop>
-        <source src="/public/animated-background.mp4" type="video/mp4" />
+        <source src="/animated-background.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
     </div>
