9901327a177a85595d0b064bfaf028147a78c5a3
Antonio F. Gasco <antoniofgasco@gmail.com>
2024-09-17 14:52:52 +0100

    Small fixes in Navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -3,7 +3,7 @@ import './Navbar.scss';
 const Navbar = () => {
   return (
     <div className="navbar">
-      <p>Logo</p>
+      <p className="navbar__logo">antoniofgasco</p>
       <div className="navbar__content">
         <p>Contact</p>
         <p>Pepe</p>
