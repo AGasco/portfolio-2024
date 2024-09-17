cacfb8f14fa9b138b9f1c63b04bd5e63b649a10d
Antonio F. Gasco <antoniofgasco@gmail.com>
2024-09-17 12:48:45 +0100

    Implemented Navbar skeleton + imported Begum font

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
 import './App.css';
-import Hero from './components/Hero/Hero';
+import './fonts.css';
+import { Hero } from './components/Hero';
+import { Navbar } from './components/Navbar';
 
 function App() {
   return (
     <>
       <div className="hero">
-        {/* Navbar */}
+        <Navbar />
         <Hero />
       </div>
       <main>{/* Rest of content */}</main>
+      <div>
+        Icons made from{' '}
+        <a href="https://www.onlinewebfonts.com/icon">svg icons</a>is licensed{' '}
+        by CC BY 4.0
+      </div>
     </>
   );
 }
---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,15 @@
+import './Navbar.scss';
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <p>Logo</p>
+      <div className="navbar__content">
+        <p>Contact</p>
+        <p>Pepe</p>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
