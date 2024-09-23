bfa6a967171e252f359d7cf364392299821c8af8
Antonio F. Gasco <antoniofgasco@gmail.com>
2024-09-23 12:53:29 +0100

    Skills data json

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,5 +1,6 @@
 import clients from './clients.json';
 import projects from './projects.json';
 import quotes from './quotes.json';
+import skills from './skills.json';
 
-export { clients, projects, quotes };
+export { clients, projects, quotes, skills };
