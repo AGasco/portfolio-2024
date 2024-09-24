2b56ed08ddfd63d2686b21d4e8e97396d8aa35b7
Antonio F. Gasco <antoniofgasco@gmail.com>
2024-09-24 14:29:54 +0100

    Fixed issue in App styles

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ import {
   SKILLS_SECTION
 } from '@/constants';
 import { BrowserRouter } from 'react-router-dom';
-import './App.css';
+import './App.scss';
 import './fonts.css';
 
 function App() {
