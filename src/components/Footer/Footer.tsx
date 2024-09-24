6c73df5faa18dfe0a38b4e1e6ec02ee4dc6cf647
Antonio F. Gasco <antoniofgasco@gmail.com>
2024-09-24 14:30:08 +0100

    Basic layout and styles for Footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,12 +3,23 @@ import './Footer.scss';
 const Footer = () => {
   return (
     <div className="footer">
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Temporibus, esse
-      maiores exercitationem, et officia iste voluptatibus earum at sapiente
-      repudiandae eaque vel dicta laboriosam alias fuga numquam repellat
-      voluptas, cum animi sit nobis aspernatur rerum? Fugiat, ullam! Esse modi,
-      sunt doloremque repudiandae dicta molestias, nulla quos itaque eius
-      accusantium rem!
+      <div className="footer__logo">antoniofgasco</div>
+      <div className="footer__links">
+        <p>About</p>
+        <p>Projects</p>
+        <p>Clients</p>
+        <p>Skills</p>
+        <p>Contact</p>
+      </div>
+      <div className="footer__socials">
+        <p>Icon 1</p>
+        <p>Icon 2</p>
+        <p>Icon 3</p>
+        <p>Icon 4</p>
+      </div>
+      {/* <div className="footer__createdWithLove">
+        {'Created with <3 in the Canary Islands'}
+      </div> */}
     </div>
   );
 };
