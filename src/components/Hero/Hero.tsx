3a736d956230091b9c340536524e50f1c7881f00
Antonio F. Gasco <antoniofgasco@gmail.com>
2024-10-09 10:56:28 +0100

    Fixed breakpoints logic for Hero + fixed wrong media query in QuoteGen

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,15 +1,20 @@
 import { CurvedArrow } from '@/components';
+import { BREAKPOINT_MOBILE, BREAKPOINT_TABLET } from '@/constants';
+import { useDeviceType } from '@/hooks';
+import { useEffect, useState } from 'react';
 import './Hero.scss';
 import HeroVideo from './HeroVideo';
 import QuoteGenerator from './QuoteGenerator';
-import { useEffect, useState } from 'react';
 
 const Hero = () => {
   const [isLoaded, setLoaded] = useState(false);
+  const device = useDeviceType();
+
+  console.log('device', device);
 
   useEffect(() => {
-    const heroArrow = document.querySelector('.hero-arrow');
-    setLoaded(Boolean(heroArrow));
+    const heroContent = document.querySelector('.hero-content');
+    setLoaded(Boolean(heroContent));
   }, []);
 
   return (
@@ -19,12 +24,25 @@ const Hero = () => {
         <div className="headings">
           <h1>Hey, I am Antonio</h1>
           <h2>a frontend developer</h2>
-          <CurvedArrow className={`hero-arrow ${isLoaded ? 'animate' : ''}`} />
-          <span className={`hero-arrow-message ${isLoaded ? 'animate' : ''}`}>
-            * some inspiring quotes for you
-          </span>
+
+          {device !== BREAKPOINT_MOBILE && device !== BREAKPOINT_TABLET && (
+            <>
+              <CurvedArrow
+                className={`hero-arrow ${isLoaded ? 'animate' : ''}`}
+              />
+              <span
+                className={`hero-arrow-message ${isLoaded ? 'animate' : ''}`}
+              >
+                * some inspiring quotes for you
+              </span>
+            </>
+          )}
         </div>
-        <QuoteGenerator isLoaded={isLoaded} />
+
+        {/* Maybe put it below the heading on mobile */}
+        {device !== BREAKPOINT_MOBILE && device !== BREAKPOINT_TABLET && (
+          <QuoteGenerator isLoaded={isLoaded} />
+        )}
       </div>
     </div>
   );
