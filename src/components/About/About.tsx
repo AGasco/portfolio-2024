d891aab9c8a2943c23fcbad4e2e31a81d16c800f
Antonio F. Gasco <antoniofgasco@gmail.com>
2024-10-21 15:14:10 +0100

    About - Responsive Styles

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,4 +1,5 @@
-import { useInView } from '@/hooks';
+import { BREAKPOINT_XLDESKTOP } from '@/constants';
+import { useDeviceType, useInView } from '@/hooks';
 import { useRef } from 'react';
 import './About.scss';
 
@@ -6,13 +7,23 @@ const triggerPointEnter = window.innerHeight;
 
 const About = () => {
   const ref = useRef<HTMLDivElement>(null);
-  const isInView = useInView(ref, triggerPointEnter);
+  const device = useDeviceType();
+  console.log('device', device);
+  const finalTrigger =
+    device !== BREAKPOINT_XLDESKTOP
+      ? triggerPointEnter * 0.95
+      : triggerPointEnter;
+  const isInView = useInView(ref, finalTrigger);
 
   return (
     <div ref={ref} className={`about ${isInView ? 'animate' : ''}`}>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, corporis
-      repellendus? Cumque qui quos aperiam cum provident, optio ab, atque itaque
-      incidunt cupiditate quo magnam saepe illum dignissimos quisquam et.
+      I'm a frontend developer who quickly adapts to new concepts, projects and
+      technologies, consistently delivering quality results. I take great
+      responsibility for my work and thrive in collaborative environments,
+      always ready to support my team with a positive attitude. <br />
+      <br />
+      With a strong mix of technical expertise and effective communication
+      skills, I aim to contribute meaningfully to any tech team.
     </div>
   );
 };
