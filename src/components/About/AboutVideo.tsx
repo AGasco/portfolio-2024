4b6aadc939980b18805a99f320b159496a48e80e
Antonio F. Gasco <antoniofgasco@gmail.com>
2024-10-24 13:55:43 +0100

    AboutVideo animates just once

--- src/components/About/AboutVideo.tsx
+++ src/components/About/AboutVideo.tsx
@@ -1,10 +1,17 @@
+import { useEffect, useState } from 'react';
 import './AboutVideo.scss';
 
 const AboutVideo = ({ isLoaded = true }: { isLoaded?: boolean }) => {
+  const [isActive, setActive] = useState(false);
+
+  useEffect(() => {
+    if (isLoaded && !isActive) setActive(true);
+  }, [isLoaded]);
+
   return (
     <div>
       <video
-        className={`about-video ${isLoaded ? 'animate' : ''}`}
+        className={`about-video ${isActive || isLoaded ? 'animate' : ''}`}
         autoPlay
         muted
         loop
